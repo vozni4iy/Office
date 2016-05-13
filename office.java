43ac561551de9b1ca83a2adeb73f951ac38f7f7a
Igor Skakun <skakuniu@gmail.com>
2016-05-13 16:17:45 +0300

    Initial commit

---
+++ office.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Admin on 13.05.2016.
+ */
+public class office {
+    public static void main (String [] args)
+    {
+        System.out.println("Office");
+        System.out.println("New");
+    }
+}
