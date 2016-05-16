3e37a8736bba8ac6b5de16dd6d38ede2819fa26f
Igor Skakun <skakuniu@gmail.com>
2016-05-16 12:06:17 +0300

    Test

--- Person.java
+++ Person.java
@@ -4,14 +4,101 @@
 public class Person {
 
     private int id;
+    private boolean is_director;
+    private boolean is_accountant;
+    private boolean is_manager;
+    private boolean is_tester;
+    private boolean is_programmer;
+    private boolean is_designer;
+    private boolean is_cleaner;
 
     public Person (int id)
     {
         this.id = id;
+        this.is_director = false;
+        this.is_accountant = false;
+        this.is_cleaner = false;
+        this.is_designer = false;
+        this.is_manager = false;
+        this.is_programmer = false;
+        this.is_tester = false;
     }
+
+    public Person (int id, boolean is_director, boolean is_accountant, boolean is_cleaner, boolean is_designer,
+                   boolean is_manager, boolean is_programmer, boolean is_tester)
+    {
+        this.id = id;
+        this.is_director = is_director;
+        this.is_accountant = is_accountant;
+        this.is_cleaner = is_cleaner;
+        this.is_designer = is_designer;
+        this.is_manager = is_manager;
+        this.is_programmer = is_programmer;
+        this.is_tester = is_tester;
+    }
+
+    public void setIs_director(boolean is_director) {
+        this.is_director = is_director;
+    }
+
+    public void setIs_accountant(boolean is_accountant) {
+        this.is_accountant = is_accountant;
+    }
+
+    public void setIs_manager(boolean is_manager) {
+        this.is_manager = is_manager;
+    }
+
+    public void setIs_tester(boolean is_tester) {
+        this.is_tester = is_tester;
+    }
+
+    public void setIs_programmer(boolean is_programmer) {
+        this.is_programmer = is_programmer;
+    }
+
+    public void setIs_designer(boolean is_designer) {
+        this.is_designer = is_designer;
+    }
+
+    public void setIs_cleaner(boolean is_cleaner) {
+        this.is_cleaner = is_cleaner;
+    }
+
+
+
+
 
     public int getId ()
     {
         return id;
+    }
+
+    public boolean is_director() {
+        return is_director;
+    }
+
+    public boolean is_accountant() {
+        return is_accountant;
+    }
+
+    public boolean is_manager() {
+        return is_manager;
+    }
+
+    public boolean is_tester() {
+        return is_tester;
+    }
+
+    public boolean is_programmer() {
+        return is_programmer;
+    }
+
+    public boolean is_designer() {
+        return is_designer;
+    }
+
+    public boolean is_cleaner() {
+        return is_cleaner;
     }
 }
--- office.java
+++ office.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Admin on 13.05.2016.
  */
@@ -5,7 +8,42 @@ public class office {
     public static void main (String [] args)
     {
         System.out.println("Office");
-        System.out.println("New");
-        System.out.println("New");
+        int quan;
+        Person person;
+        List<Person> staff = new ArrayList<Person>();
+        quan = (int) (Math.random() * 91) + 10;
+        System.out.println(quan);
+        person = new Person(1,true,false,false,false,false,false,false);
+        staff.add(person);
+        person = new Person(2,false,true,false,false,false,false,false);
+        staff.add(person);
+        person = new Person(3,false,false,false,false,true,false,false);
+        staff.add(person);
+        person = new Person(4,false,false,true,false,false,false,false);
+        staff.add(person);
+        printStaff(staff);
+    }
+
+    public static void printStaff (List<Person> staff)
+    {
+        for (Person person : staff)
+        {
+            System.out.print(person.getId() + " ");
+            if (person.is_director())
+                System.out.print("Директор ");
+            if (person.is_accountant())
+                System.out.print("Бухгалтер ");
+            if (person.is_cleaner())
+                System.out.print("Уборщик ");
+            if (person.is_designer())
+                System.out.print("Дизайнер ");
+            if (person.is_manager())
+                System.out.print("Менеджер ");
+            if (person.is_programmer())
+                System.out.print("Программист ");
+            if (person.is_tester())
+                System.out.print("Тестировщик");
+            System.out.println();
+        }
     }
 }
